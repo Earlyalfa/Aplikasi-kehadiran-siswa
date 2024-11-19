eaed0530646d55bc5c631b9932be139b5ce42586
Earlyalfa <ealfasheilawati@gmail.com>
2024-11-19 11:36:40 +0700

    Aplikasi_kehadiran

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:kehadiran_siswa/provider/kehadiran_provider.dart';
+import 'package:provider/provider.dart';
+import './providers/kehadiran_provider.dart';
+import './views/home_screen.dart';
+
+void main() => runApp(const AplikasiKehadiranSiswa());
+
+class AplikasiKehadiranSiswa extends StatelessWidget {
+  const AplikasiKehadiranSiswa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => KehadiranProvider(),
+      child: MaterialApp(
+        title: 'Aplikasi Kehadiran Siswa',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
